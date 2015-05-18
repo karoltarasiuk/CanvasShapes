4aee8bb346e4a18dbe12371243a0a2ec7d64ef5e
karoltarasiuk <carlostarasiuk@gmail.com>
2015-05-18 19:19:19 +1000

    [POLISH] Better doc comments + change of method name

--- src/Tools.js
+++ src/Tools.js
@@ -3,7 +3,19 @@
 CanvasShapes.Tools = (function () {
 
     /**
+     * Calculates the measure of an angle based on 3 points creating it. By
+     * default it returns measure in degrees but it can return it in radians. To
+     * do so simply pass `true` as fourth parameter. Coordinates of a point are
+     * arrays in the following format: `[x, y]`.
+     *
      * From law of cosines: http://en.wikipedia.org/wiki/Law_of_cosines
+     *
+     * @param {array}   p1      coordinates of apex
+     * @param {array}   p2      coordinates
+     * @param {array}   p3      coordinates
+     * @param {boolean} radians [OPTIONAL]
+     *
+     * @return {float}
      */
     function angleMeasure (p1, p2, p3, radians) {
 
@@ -18,17 +30,38 @@ CanvasShapes.Tools = (function () {
             return r;
         }
 
-        return radiansToDegree(r);
+        return radiansToDegrees(r);
     }
 
+    /**
+     * Converts degrees measure to radians.
+     *
+     * @param  {float} degress
+     * @return {float}
+     */
     function degreesToRadians (degress) {
         return degress * Math.PI / 180;
     }
 
-    function radiansToDegree (radians) {
+    /**
+     * Converts radians measure to degrees.
+     *
+     * @param  {float} radians
+     * @return {float}
+     */
+    function radiansToDegrees (radians) {
         return radians * 180 / Math.PI;
     }
 
+    /**
+     * Calculates a length of a segment defined by two points. Coordinates of a
+     * point are arrays in the following format: `[x, y]`.
+     *
+     * @param  {array} p1 coordinates
+     * @param  {array} p2 coordinates
+     *
+     * @return {float}
+     */
     function segmentLength(p1, p2) {
 
         return Math.sqrt(
@@ -40,7 +73,7 @@ CanvasShapes.Tools = (function () {
     return {
         angleMeasure: angleMeasure,
         segmentLength: segmentLength,
-        radiansToDegree: radiansToDegree,
+        radiansToDegrees: radiansToDegrees,
         degreesToRadians: degreesToRadians
     };
 })();
